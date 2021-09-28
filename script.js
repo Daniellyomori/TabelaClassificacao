5efbffa5a6d792dd47c237e44d7c4b63c8906edf
Danielly Omori Antunes de Oliveira <50346193+Daniellyomori@users.noreply.github.com>
2021-09-28 20:17:18 -0300

    Commit Inicial

---
+++ script.js
@@ -0,0 +1,65 @@
+var jogador1 = {
+  nome: "Daninha",
+  vitorias: 0,
+  empates: 0,
+  derrotas: 0,
+  pontos: 0
+};
+
+var jogador2 = {
+  nome: "Vininho",
+  vitorias: 0,
+  empates: 0,
+  derrotas: 0,
+  pontos: 0
+};
+
+function calculaPontos(jogador) {
+  var pontos = jogador.vitorias * 3 + jogador.empates;
+  return pontos;
+}
+
+var jogadores = [jogador1, jogador2];
+
+function exibeJogadoresNaTela(jogadores) {
+  var elemento = "";
+  for (var i = 0; i < jogadores.length; i++) {
+    elemento += "<tr><td>" + jogadores[i].nome + "</td>";
+    elemento += "<td>" + jogadores[i].vitorias + "</td>";
+    elemento += "<td>" + jogadores[i].empates + "</td>";
+    elemento += "<td>" + jogadores[i].derrotas + "</td>";
+    elemento += "<td>" + jogadores[i].pontos + "</td>";
+    elemento +=
+      "<td><button onClick='adicionarVitoria(" + i + ")'>Vitória</button></td>";
+    elemento +=
+      "<td><button onClick='adicionarEmpate(" + i + ")'>Empate</button></td>";
+    elemento +=
+      "<td><button onClick='adicionarDerrota(" + i + ")'>Derrota</button></td>";
+    elemento += "</tr>";
+  }
+
+  var tabelaJogadores = document.getElementById("tabelaJogadores");
+  tabelaJogadores.innerHTML = elemento;
+}
+
+exibeJogadoresNaTela(jogadores);
+
+function adicionarVitoria(i) {
+  var jogador = jogadores[i];
+  jogador.vitorias++;
+  jogador.pontos = calculaPontos(jogador);
+  exibeJogadoresNaTela(jogadores);
+}
+
+function adicionarEmpate(i) {
+  var jogador = jogadores[i];
+  jogador.empates++;
+  jogador.pontos = calculaPontos(jogador);
+  exibeJogadoresNaTela(jogadores);
+}
+
+function adicionarDerrota(i) {
+  var jogador = jogadores[i];
+  jogador.derrotas++;
+  exibeJogadoresNaTela(jogadores);
+}
